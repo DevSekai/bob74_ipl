5f67d9aa8d184f8d4895406f337dda890f6d9c01
TheIndra55 <theindra@protonmail.com>
2024-10-11 00:05:29 +0200

    Add all Pillbox hospital states
    Co-authored-by: Sqrl <pigbird684@gmail.com>

--- gtav/pillbox_hospital.lua
+++ gtav/pillbox_hospital.lua
@@ -6,7 +6,36 @@ end)
 PillboxHospital = {
     ipl = "rc12b_default",
 
+    State = {
+        default = "rc12b_default",
+        fixed = "rc12b_fixed",
+        destroyed = {
+            "rc12b_destroyed",
+            "rc12b_hospitalinterior"
+        },
+
+        Set = function(state)
+            PillboxHospital.State.Clear()
+
+            EnableIpl(state, true)
+        end,
+        Clear = function()
+            EnableIpl({
+                PillboxHospital.State.default,
+                PillboxHospital.State.fixed,
+                PillboxHospital.State.destroyed
+            }, false)
+        end
+    },
+
+    -- Kept for backwards compatibility
     Enable = function(state)
+        PillboxHospital.State.Clear()
+
         EnableIpl(PillboxHospital.ipl, state)
+    end,
+
+    LoadDefault = function()
+        PillboxHospital.State.Set(PillboxHospital.State.default)
     end
 }
